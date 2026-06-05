6e79abbdb252844fc7c22f334cd3df992b589c24
schoolonnie <smithlonniedrew@gmail.com>
2026-06-05 09:16:06 -0500

    created initial List Entry class for server entry objects

---
+++ scripts/lists.js
@@ -0,0 +1,10 @@
+class ListEntry {
+    constructor(name, ip, players, maxPlayers, version, type) {
+        this.name = name;
+        this.ip = ip;
+        this.players = players;
+        this.maxPlayers = maxPlayers;
+        this.version = version;
+        this.type = type;
+    }
+}
